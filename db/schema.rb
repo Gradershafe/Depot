c0769ee102b384cd64559812a7bf38503d7829f8
Gradershafe <kisilto@gmail.com>
2016-04-19 22:53:06 +0300

    a little of JQuery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326180458) do
+ActiveRecord::Schema.define(version: 20160328153212) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
